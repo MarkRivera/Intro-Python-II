5c86f667357d806828066841b4367ab5aa161d50
Mark Rivera <Mriver1991@gmail.com>
2020-09-07 20:07:11 -0400

    finished game

--- src/adv.py
+++ src/adv.py
@@ -7,19 +7,13 @@ room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
-    'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+    'foyer':    Room("Foyer", "Dim light filters in from the south. Dusty passages run north and east."),
 
-    'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
-into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+    'overlook': Room("Grand Overlook", "A steep cliff appears before you, falling into the darkness. Ahead to the north, a light flickers in the distance, but there is no way across the chasm."),
 
-    'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+    'narrow':   Room("Narrow Passage", "The narrow passage bends here from west to north. The smell of gold permeates the air."),
 
-    'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
-chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+    'treasure': Room("Treasure Chamber", "You've found the long-lost treasure chamber! Sadly, it has already been completely emptied by earlier adventurers. The only exit is to the south."),
 }
 
 
@@ -44,7 +38,12 @@ class Player:
         self.name = name
         self.location = location
 
+    def where(self):
+        print(f"{self.location.name}")
+
 Player_1 = Player("Mark", room["outside"])
+## Player_1.location = Player_1.location.n_to
+Player_1.where()
 
 # Write a loop that:
 #
@@ -62,12 +61,46 @@ def formatRoomDesc(description):
     formatted_text = textwrapper.wrap(description)
     return [print(text) for text in formatted_text]
 
-def game_loop():
+def movePlayer():
     print(Player_1.location.name)
-    formatRoomDesc(Player_1.location.desc)
-    
     # Waiting For User Input
-    userInput = input("Choose a direction to walk in... \n")
+    userInput = input("Choose a direction (N, E, W, or S) to walk in... \n").lower()
 
+    if userInput == "n":
+        try:
+            Player_1.location = Player_1.location.n_to
+        except AttributeError:
+            print("You find that you cannot move any further North")
+        
+    elif userInput == "e":
+        try:
+            Player_1.location = Player_1.location.e_to
+        except AttributeError:
+            print("You find that you cannot move any further East")
+    
+    elif userInput == "w":
+        try:
+            Player_1.location = Player_1.location.w_to
+        except AttributeError:
+            print("You find that you cannot move any further West")
+
+    elif userInput == "s":
+        try:
+            Player_1.location = Player_1.location.s_to
+        except AttributeError:
+            print("You find that you cannot move any further South")
+    
+    elif userInput == "q":
+        exit()
+
+    else:
+        return "Invalid option, please choose a cardinal direction (N, E, W, S) or quit (Q)"
+
+    return
+
+def game_loop():
+    formatRoomDesc(Player_1.location.desc)
+    movePlayer()
+    game_loop()
 
 game_loop()
